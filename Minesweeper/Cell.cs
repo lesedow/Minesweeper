c75085680a7f2455652cca0bec2774dc0f5e38a7
lesedow <lesedow@gmai.com>
2025-07-12 13:48:20 +0300

    Moved cell logic in its own class

---
+++ Minesweeper/Cell.cs
@@ -0,0 +1,44 @@
+﻿namespace MinesweeperGame
+{
+    internal class Cell
+    {
+        private int _bombNeighbours;
+        private bool isBomb;
+        private bool isRevealed;
+
+        private int _x, _y;
+
+        public Cell(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _bombNeighbours = 0;
+            isBomb = false;
+            isRevealed = false;
+        }
+        
+        public void SetToBomb() => isBomb = true;
+        public void Reveal() => isRevealed = true;
+
+        public void CountBombNeighbours(Board board)
+        {
+            if (isBomb) return;
+
+            Cell[,] grid = board.GetGrid();
+
+            int minX = Math.Min(0, _x - 1);
+            int maxX = Math.Max(grid.GetLength(1), _x + 1);
+            int minY = Math.Min(0, _y - 1);
+            int maxY = Math.Max(grid.GetLength(0), _y + 1);
+
+            for (int cellY = minY; cellY < maxY; cellY++)
+            {
+                for (int cellX = minX; cellX < maxX; cellX++)
+                {
+                    if (!board.GetCellAt(cellX, cellY).isBomb) continue;
+                    _bombNeighbours++;
+                }
+            }
+        }
+    }
+}
