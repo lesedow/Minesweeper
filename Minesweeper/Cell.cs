2fcfb16a07e8d4267b71af8c7a688bf0ad0c8edc
lesedow <lesedow@gmai.com>
2025-07-15 01:26:58 +0300

    Added proper getters and setters

--- Minesweeper/Cell.cs
+++ Minesweeper/Cell.cs
@@ -2,42 +2,30 @@
 {
     internal class Cell
     {
-        private int _bombNeighbours;
-        private bool _isBomb;
-        private bool _isRevealed;
         private Board _boardRef;
-
-        private int _x, _y;
+        public int x { get; private set; }
+        public int y { get; private set; }
+        public int BombNeighbours { get; private set; } = 0;
+        private bool Revealed { get; set; } = false;
+        private bool Bomb { get; set; } = false;
 
         public Cell(int x, int y, Board boardRef)
         {
-            _x = x;
-            _y = y;
-            _bombNeighbours = 0;
-            _isBomb = false;
-            _isRevealed = false;
+            this.x = x;
+            this.x = y;
             _boardRef = boardRef;
-        }
-        public bool IsRevealed() => _isRevealed;
-        public bool IsBomb() => _isBomb;
-        public int GetBombNeighbours() => _bombNeighbours;
-        public void IncreaseNumberOfNeighbours() => _bombNeighbours++;
-        public void SetToBomb(Board board)
-        {
-            _isBomb = true;
-            CountBombNeighbours();
         }
 
         public void Reveal()
         {
-            _isRevealed = true;
+            Revealed = true;
 
-            if (IsBomb() || GetBombNeighbours() > 0) return;
+            if (!Bomb || BombNeighbours > 0) return;
             
-            int minX = Math.Max(0, _x - 1);
-            int maxX = Math.Min(_boardRef.GetColSize() - 1, _x + 1);
-            int minY = Math.Max(0, _y - 1);
-            int maxY = Math.Min(_boardRef.GetRowSize() - 1, _y + 1);
+            int minX = Math.Max(0, x - 1);
+            int maxX = Math.Min(_boardRef.GetColSize() - 1, x + 1);
+            int minY = Math.Max(0, y - 1);
+            int maxY = Math.Min(_boardRef.GetRowSize() - 1, y + 1);
 
             for (int cellY = minY; cellY <= maxY; cellY++)
             {
@@ -45,8 +33,8 @@
                 {
                     Cell cellAtPos = _boardRef.GetCellAt(cellX, cellY);
 
-                    if (cellAtPos.IsBomb() ||  
-                        cellAtPos.IsRevealed()
+                    if (cellAtPos.Bomb ||  
+                        cellAtPos.Revealed
                     ) continue;
 
                     cellAtPos.Reveal();
@@ -55,20 +43,20 @@
 
         }
 
-        private void CountBombNeighbours()
+        public void CountBombNeighbours()
         {
-            int minX = Math.Max(0, _x - 1);
-            int maxX = Math.Min(_boardRef.GetColSize() - 1, _x + 1);
-            int minY = Math.Max(0, _y - 1);
-            int maxY = Math.Min(_boardRef.GetRowSize() - 1, _y + 1);
+            int minX = Math.Max(0, x - 1);
+            int maxX = Math.Min(_boardRef.GetColSize() - 1, x + 1);
+            int minY = Math.Max(0, y - 1);
+            int maxY = Math.Min(_boardRef.GetRowSize() - 1, y + 1);
 
             for (int cellY = minY; cellY <= maxY; cellY++)
             {
                 for (int cellX = minX; cellX <= maxX; cellX++)
                 {
                     Cell cellAtPos = _boardRef.GetCellAt(cellX, cellY);
-                    if (cellAtPos.IsBomb()) continue;
-                    cellAtPos.IncreaseNumberOfNeighbours();
+                    if (cellAtPos.Bomb) continue;
+                    cellAtPos.BombNeighbours++;
                 }
             }
         }
